235853c1d8b5eb006f95458829b78df79e6ca09a
fazzyA <faz.pak@gmail.com>
2020-06-11 23:33:20 +0500

    balance add

---
+++ src/components/Balance.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Balance = ()=>{
+    const total=0;
+    return (
+        <>
+          <h4>Your Balance</h4>
+        <h1>${total}</h1>
+        </>
+      )
+}
